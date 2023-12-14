4794cd6e0093ca21e218bdddcd3b434a1016dc7a
matteomedori <matteo.medori.97@gmail.com>
2023-12-14 12:44:45 +0100

    feat: aggiunto componente searchbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,26 @@
 <script>
+import SearchBar from "./SearchBar.vue";
 export default {
   name: "AppHeader",
+  components: {
+    SearchBar,
+  },
 };
 </script>
 
 <template>
   <header>
     <h1>Boolflix</h1>
+    <SearchBar />
   </header>
 </template>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+
+  h1 {
+    margin: 20px 0;
+  }
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
+
+<template>
+  <div class="searchbar">
+    <input type="text" placeholder="Search.." name="search" />
+    <button type="submit">Vai</button>
+  </div>
+</template>
+
+<style></style>
