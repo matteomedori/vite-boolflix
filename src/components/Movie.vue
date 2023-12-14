53bc8511b352c2a903a6b25d6592e24d2bf4f71d
matteomedori <matteo.medori.97@gmail.com>
2023-12-14 15:55:40 +0100

    feat: milestone 2 gestione lingua e bandiera

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -2,6 +2,11 @@
 export default {
   name: "Movie",
   props: ["title", "originalTitle", "originalLanguage", "vote", "movieIndex"],
+  methods: {
+    getImagePath(url) {
+      return new URL(url, import.meta.url).href;
+    },
+  },
 };
 </script>
 
@@ -10,7 +15,17 @@ export default {
     Film {{ movieIndex + 1 }}:
     <div>Titolo: {{ title }}</div>
     <div>Titolo originale: {{ originalTitle }}</div>
-    <div>Lingua: {{ originalLanguage }}</div>
+    <div v-show="originalLanguage === 'en'">
+      Lingua:
+      <img :src="getImagePath('../assets/img/uk_flag.png')" alt="#" />
+    </div>
+    <div v-show="originalLanguage === 'it'">
+      Lingua:
+      <img :src="getImagePath('../assets/img/italy_flag.png')" alt="#" />
+    </div>
+    <div v-show="originalLanguage !== 'it' && originalLanguage !== 'en'">
+      Lingua: {{ originalLanguage }}
+    </div>
     <div>Voto: {{ vote }}</div>
   </li>
 </template>
@@ -18,5 +33,10 @@ export default {
 <style scoped>
 li {
   margin-bottom: 10px;
+}
+
+img {
+  max-width: 25px;
+  height: 15px;
 }
 </style>
